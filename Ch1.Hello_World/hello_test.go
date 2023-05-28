b9e573f427efd47ac9b455598e27cfbca223f9d8
helin1995 <helin.nlp@gmail.com>
2023-05-28 23:45:34 +0800

    重构代码并且使用switch case

--- Ch1.Hello_World/hello_test.go
+++ Ch1.Hello_World/hello_test.go
@@ -10,16 +10,9 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
-
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+	t.Run("is Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 
 		assertCorrectMessage(t, got, want)
 	})
